24f338c0825624550d589c367ba5146b5be23549
keiryan wilson <keiryanwilson@gmail.com>
2024-07-03 00:12:02 -0400

    added new chat transition

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="w-full flex justify-between items-center text-sm dark-hover px-4 py-2 min-w-72 transition-custom">
+  <div
+    class="w-full flex justify-between items-center text-sm chat-transition hover:bg-[#FFFFFF1D] cursor-pointer px-4 py-2 min-w-72"
+    :class="!navOpen && ' -translate-x-36 opacity-0'"
+    :style="{ transitionDelay: `${index * 20}ms` }"
+  >
     <!-- Profile Image -->
-    <div class="rounded-full bg-red-500 w-8 h-8 mr-2 overflow-hidden">
+    <div
+      class="rounded-full bg-red-500 w-8 h-8 mr-2 overflow-hidden transition-transform"
+    >
       <!-- <img
-        class="w-full rounded-full"
-        :src="chat.assistant.avatar"
-        alt="Profile Image"
-      /> -->
+          class="w-full rounded-full"
+          :src="chat.assistant.avatar"
+          alt="Profile Image"
+        /> -->
     </div>
 
     <!-- Chat title -->
@@ -29,12 +35,17 @@ export default {
   components: {},
   props: {
     chat: Object,
+    navOpen: Boolean,
+    index: Number,
   },
   data() {
     return {};
   },
   computed: {},
   methods: {},
+  mounted() {
+    console.log(this.index, this.index * 10);
+  },
 };
 </script>
 
---
+++ src/components/MessageAll.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="w-screen h-screen fixed opacity-0 transition-custom"
+    :class="messageAllOpen ? 'opacity-100 pointer-events-all' : 'pointer-events-none'"
+  >
+    <!-- Background blur -->
+    <div
+      class="w-full h-full absolute top-0 left-0 backdrop-blur backdrop-brightness-[0.20] hover:cursor-pointer"
+      @click="$emit('toggle-app-settings')"
+    ></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<script>
+export default {
+  name: "MessageAll",
+  components: {},
+  props: {
+    messageAllOpen: Boolean,
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  created() {},
+  updated() {},
+  destroyed() {},
+};
+</script>
